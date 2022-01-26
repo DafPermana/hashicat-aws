5b01d1e6c15d6651a887e7b23563445e86eb9c38
DafPermana <daffa@ivteknologi.com>
2022-01-26 16:29:52 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -128,8 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    department    = "devops"
-    billable      = "true"
+    Department    = "devops"
+    Billable      = "true"
   }
 }
 
